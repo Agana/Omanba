4d5a6750f01f5e8d8c6bf79983c87bf40cb48df8
aiti <aiti@sel-Aspire-3410.(none)>
2011-07-06 13:56:01 +0000

    changes

--- omanba/omanba_app/models.py
+++ omanba/omanba_app/models.py
@@ -2,38 +2,17 @@
 from django.db import models
 from django.contrib import admin
 
+
+
 class Article(models.Model):
 	title= models.CharField(max_length=200)
 	headline=models.CharField(max_length=300)
 	date_created=models.DateTimeField('date published')
-	article_options=((news, News), (lifestyle, Lifestyle), (politics, Politics), (extras, Extras), (sports, Sports))
-	article_type=models.CharField(max_length=6, choices=article_options)
+	STATUS_CHOICES=(('news', 'News'), ('lifestyle', 'Lifestyle'), ('politics', 'Politics'), ('extras', 'Extras'), ('sports', 'Sports'),)
+	article_type=models.CharField(max_length=6, choices=STATUS_CHOICES)
 	def __unicode__(self):
 		return self.title
 
-class News(models.Model):
-	news_title=models.CharField(max_length=200)
-	news_Headlines=models.CharField(max_length=300)
-	news_date=models.DateTimeField('date published')
-	news_body=models.TextField()
-	def __unicode__(self):
-		return self.news_title
-
-class Sports(models.Model):
-	sports_title=models.CharField(max_length=200)
-	sports_Headlines=models.CharField(max_length=300)
-	sports_date=models.DateTimeField('date published')
-	sports_body=models.TextField()
-	def __unicode__(self):
-		return self.sports_title
-
-class Politics(models.Model):
-	politics_title=models.CharField(max_length=200)
-	politics_Headlines=models.CharField(max_length=300)
-	politics_date=models.DateTimeField('date published')
-	politics_body=models.TextField()
-	def __unicode__(self):
-		return self.politics_title
 
 class Extras(models.Model):
 	extras_title=models.CharField(max_length=200)
@@ -57,26 +36,13 @@ class Donate(models.Model):
 	def __unicode__(self):
 		return self.project_title
 
-class Lifestyle(models.Model):
-	lifest_title=models.CharField(max_length=200)
-	lifest_body=models.TextField()
-	lifest_author=models.CharField(max_length=60)
-	def __unicode__(self):
-		return self.lifest_title
-	
 
 
-class NewsAdmin(admin.ModelAdmin):
-	list_display=('news_title','news_Headlines','news_body')
 
-class SportsAdmin(admin.ModelAdmin):
-	list_display=('sports_title','sports_Headlines','sports_body')
 
-class PoliticsAdmin(admin.ModelAdmin):
-	list_display=('politics_title','politics_Headlines','politics_body')
 
 class ExtrasAdmin(admin.ModelAdmin):
-	list_display=('extras_title','extras_Headlines','extras_body')
+	list_display=('extras_title','extras_headlines','extras_body')
 
 class Gh_VidAdmin(admin.ModelAdmin):
 	list_display=('gh_vid_title','gh_vid_body','gh_file')
@@ -84,13 +50,10 @@ class Gh_VidAdmin(admin.ModelAdmin):
 class DonateAdmin(admin.ModelAdmin):
 	list_display=('project_title','project_author','project_body')
 
-class LifestyleAdmin(admin.ModelAdmin):
-	list_display=('lifest_title','lifest_author','lifest_body')
 
-admin.site.register(News,NewsAdmin)
-admin.site.register(Politics,PoliticsAdmin)
+
+
 admin.site.register(Gh_Vid,Gh_VidAdmin)
 admin.site.register(Extras,ExtrasAdmin)
 admin.site.register(Donate,DonateAdmin)
-admin.site.register(Sports,SportsAdmin)
-admin.site.register(Lifestyle,LifestyleAdmin)
+admin.site.register(Article)
--- omanba/urls.py
+++ omanba/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'omanba.views.home', name='home'),
+    #url(r'^$', 'omanba.views.home', name='home'),
     # url(r'^omanba/', include('omanba.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -14,4 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^omanba/', include('omanba.omanba_app.urls')),
 )
