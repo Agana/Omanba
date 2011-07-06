79a54106d8119577a523152e451dfbe1d5fb9b86
Amoako Radjwan <nanayawrid1@yahoo.com>
2011-07-06 03:20:08 +0000

    rid merge

--- omanba/omanba_app/views.py
+++ omanba/omanba_app/views.py
@@ -1,1 +1,6 @@
 # Create your views here.
+from models import Article,Extras,Gh_Vid,Donate
+from django.http import HttpResponds
+from django.template import Context,loader
+
+def Articles(request):
