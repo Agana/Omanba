883a5e42d26ca7f28d90c518ef505078e201c87f
Amoako Radjwan <nanayawrid1@yahoo.com>
2011-07-07 04:22:48 +0000

    Amoako

--- omanba/omanba_app/views.py
+++ omanba/omanba_app/views.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
+
 # Create your views here.
-from models import Article,Extras,Gh_Vid,Donate
-from django.http import HttpResponds
-from django.template import Context,loader
-=======
+
 from django.template import Context, loader
 from django.http import HttpResponse
 from models import Gh_Vid, Donate,Extras, Article
@@ -11,19 +8,36 @@ from django.template import Context, loader
 from django.forms import ModelForm
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect
->>>>>>> 34fb685c878539a18e830eacc34a76d8712cc3a5
 
 def Article_list(request,Show_Article=False):
 	Article_list=Article.objects.all()
 	print Article_list
-	if Show_Article:
-		Show=Article.object.get(pk=id)
-		print Show
 	a=loader.get_template('omanba_app/list.html')
 	c=Contect({'Article_list':Article_list})
+
+
+def Article_detail(request,Show_Article=False):
+	if Show_Article:
+		Show=Article.object.filter(pk=id)
+		print Show
 def Extra_list(request):
+	Extra_list=Extra.objects.all()
+return HttpResponse(t.render(c))
+
+
 def Extra_details(request):
+
+return HttpResponse(t.render(c))
+
 def Donate_detial(request):
+return HttpResponse(t.render(c))
+
+
 def Index(request):
+	t = loader.get_template('omanba/base.html')
+	c = Context(dict())
+	return HttpResponse(t.render(c))
+	
+
 
 	
